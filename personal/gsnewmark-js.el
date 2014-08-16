9e3c78e52c434aecefad61a4a1c435153681da36
Ivan Kryvoruchko <gildraug@gmail.com>
2014-08-16 16:49:42 +0300

    Tune JavaScript config.

--- personal/gsnewmark-js.el
+++ personal/gsnewmark-js.el
@@ -9,13 +9,21 @@
 
 (prelude-require-packages '(js2-mode company-tern tern skewer-mode))
 
-(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
+(js2-imenu-extras-mode)
+
 (eval-after-load 'tern
   '(progn
+     (setq company-tern-property-marker "")
      (set (make-local-variable 'company-backends) '(company-tern))))
+
+(eval-after-load 'skewer-mode
+  '(progn
+     (set (make-local-variable 'httpd-port) 4242)))
 
 (add-hook 'js2-mode-hook 'skewer-mode)
 (add-hook 'css-mode-hook 'skewer-css-mode)
