3ca50697e89ab8c46bf4654745da3a7edd528bfc
Ivan Kryvoruchko <gildraug@gmail.com>
2014-08-24 13:18:45 +0300

    Add Haskell support for company-mode.

--- personal/gsnewmark-haskell.el
+++ personal/gsnewmark-haskell.el
@@ -7,16 +7,25 @@
 
 ;;; Code:
 
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(prelude-require-packages '(haskell-mode ghc company-ghc))
 
-(custom-set-variables
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-type 'cabal-repl))
+(eval-after-load 'haskell-mode
+  '(progn
+     (autoload 'ghc-init "ghc" nil t)
 
-(define-key haskell-mode-map (kbd "C-c M-j") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+     (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+     (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+     (custom-set-variables
+      '(haskell-process-suggest-remove-import-lines t)
+      '(haskell-process-auto-import-loaded-modules t)
+      '(haskell-process-log t)
+      '(haskell-process-type 'cabal-repl))
+
+     (define-key haskell-mode-map (kbd "C-c M-j") 'haskell-interactive-bring)
+     (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+
+     (add-to-list 'company-backends 'company-ghc)))
 
 (provide 'gsnewmark-haskell)
 
