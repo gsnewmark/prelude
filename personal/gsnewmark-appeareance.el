b812f09aa4a623bbd2d29ff25c9a0959182cc042
Ivan Kryvoruchko <gildraug@gmail.com>
2015-02-21 13:34:00 +0200

    Switch to Fantasque Sans Mono

--- personal/gsnewmark-appeareance.el
+++ personal/gsnewmark-appeareance.el
@@ -36,7 +36,7 @@
        sml/hidden-modes))
 
 ;; Set font
-(add-to-list 'default-frame-alist '(font . "Menlo-9"))
+(add-to-list 'default-frame-alist '(font . "Fantasque Sans Mono-10"))
 
 ;; Remove scroll bar
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
